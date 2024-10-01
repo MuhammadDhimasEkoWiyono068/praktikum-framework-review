9f630547ffd2768153f04d16e94318b36e92cb8b
Dhimas Eko 068 <dhimaseko00@gmail.com>
2024-10-01 12:15:22 +0800

    review4

--- framework/ais/urls.py
+++ framework/ais/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('about', views.about),
+    # path('about', views.about),
+    path('about/', views.about, name='about'),
     path('', views.homepage, name='homepage'),
+    path('student/', views.student_index, name='student_index'),
+    path('student/create/', views.student_create, name='student_create'),  # Create
 ]
