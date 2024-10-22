69ecc3776824d2d9ce73855154a067b864eded67
Dhimas Eko 068 <dhimaseko00@gmail.com>
2024-10-22 12:28:38 +0800

    Review 7

--- framework/ais/urls.py
+++ framework/ais/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
+from rest_framework.routers import DefaultRouter
+from .views import StudentsViewSet
+
+router = DefaultRouter() # Membuat router DRF
+router.register(r'students', StudentsViewSet) # Menyambungkan StudentsViewSet ke URL /students/
 
 urlpatterns = [
     # path('about', views.about),
@@ -16,4 +21,5 @@ urlpatterns = [
     path('dashboard/admin', views.dashboard_admin, name='dashboard_admin'),
     path('dashboard/student', views.dashboard_student,name='dashboard_student'),
     path('dashboard/teacher', views.dashboard_teacher,name='dashboard_teacher'),
+    path('api/', include(router.urls)), # Ini akan menambahkan semua URL yang dibutuhkan untuk API
 ]
